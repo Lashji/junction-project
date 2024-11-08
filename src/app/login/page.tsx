3e78887a45ef7cbbdc7657a33101a89e087c9b44
Lassi Palojärvi <Lassi.palojarvi@outlook.com>
2024-11-09 01:56:50 +0200

    Signicat auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { Button } from "~/components/ui/button";
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("/api/auth/sign-in", {
+        method: "POST",
+      });
+      const { url } = (await response.json()) as { url: string };
+      if (url) {
+        window.location.href = url;
+      }
+    } catch (error) {
+      console.error("Failed to initialize authentication:", error);
+    }
+  };
+  return <Button onClick={handleLogin}>Login</Button>;
 }
