5e332f7e3537460705d6d98ea79e6b09cae84308
Lassi Palojärvi <Lassi.palojarvi@outlook.com>
2024-11-09 11:19:23 +0200

    Initial navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import HomeClient from "./_components/home-client";
 import FingerprintProvider from "./_components/fingerprint-provider";
+import Navbar from "./_components/navbar";
 
 export default async function Page() {
   const cookieStore = await cookies();
@@ -8,6 +9,7 @@ export default async function Page() {
 
   return (
     <FingerprintProvider tempIdToken={tempIdToken}>
+      <Navbar />
       <HomeClient />
     </FingerprintProvider>
   );
