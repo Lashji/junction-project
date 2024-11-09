5bb3364f5e3c39766d67e1ccb0e0f0009dcf6082
Siyaarr <Siyaryucel@gmail.com>
2024-11-09 17:50:21 +0200

    asd

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,4 @@
-export type Comment = {
-  id: string;
-  text: string;
-  likes: number;
-  userLiked: boolean;
-};
 
-export type Poll = {
-  id: number;
-  question: string;
-  options: [string, string];
-  votes: [number, number];
-  comments: [Comment[], Comment[]];
-  createdAt: Date;
-  userVoted: boolean;
-  justVoted: boolean;
-};
 
 export type TokenData = {
   iss: string;
@@ -35,4 +19,57 @@ export type TokenData = {
   birthdate: string;
   idp_issuer: string;
   sandbox: boolean;
+};
+
+export type Answer = {
+  id: string;
+  userId: string;
+  pollId: string;
+  answer: string;
+  createdAt: string;
+};
+
+/**
+export type Poll = {
+  id: number;
+  question: string;
+  options: [string, string];
+  votes: [number, number];
+  comments: [Comment[], Comment[]];
+  createdAt: Date;
+  userVoted: boolean;
+  justVoted: boolean;
+};
+*/
+
+export type Poll = {
+  id: string;
+  title: string;
+  description: string;
+  options: string[];
+  answers: Answer[];
+  comments: Comment[];
+};
+
+/**
+ * export type Comment = {
+  id: string;
+  text: string;
+  likes: number;
+  userLiked: boolean;
+};
+
+ */
+
+export type Threads = Record<string, Comment[]>;
+
+export type Comment = {
+  id: string;
+  content: string;
+  pollAnswer: string;
+  userId: string;
+  pollId: string;
+  threadId: string;
+  threadPosition: number;
+  createdAt: string;
 };
