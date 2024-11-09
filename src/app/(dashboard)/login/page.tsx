c01db3a5b9d90b4e33376dec9a5e903f2edaae30
Vladislav Korniushenko <vkornushenko@gmail.com>
2024-11-10 01:56:42 +0200

    login page css

--- src/app/(dashboard)/login/page.tsx
+++ src/app/(dashboard)/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { IoMdKey } from "react-icons/io";
 
 import { Button } from "~/components/ui/button";
 
@@ -17,17 +18,35 @@ export default function LoginPage() {
     }
   };
   return (
-    <div className="container mx-auto p-4">
-      <div className="p-4 mt-6 mb-6 bg-card-foreground rounded-lg">
-        <h1 className="mb-4 text-2xl font-bold">
-          Authenticate yourself to vote
-        </h1>
-        <p className="mb-2 ">
-          You need to authenticate yourself to vote. Quorum guarantee you 100%
-          anonymity in voting process.
+    <div className="flex h-screen items-center justify-center">
+      <div className="m-16 flex flex-col items-center rounded-lg bg-card-foreground p-4">
+        <div className="mt-[-8px]">
+          <IoMdKey size={100} />
+        </div>
+        <p className="mb-2">You need to authenticate yourself to vote.</p>
+        <p className="mb-2 pb-6">
+          <b>Quorum</b> guarantee you <b>100% anonymity</b> in voting process.
         </p>
-        <Button variant="default" onClick={handleLogin}>Authenticate</Button>
+        <Button variant="default" onClick={handleLogin}>
+          Authenticate with Bank ID
+        </Button>
       </div>
+
+      {/* <div className="container mx-auto p-4">
+        <div className="content-center">
+          <IoMdKey size={70} className="content-center" />
+        </div>
+        <div className="mb-6 mt-6 rounded-lg bg-card-foreground p-4">
+          <h1 className="mb-4 text-2xl font-bold">
+            Authenticate yourself to vote
+          </h1>
+          <p className="mb-2">
+            You need to authenticate yourself to vote. Quorum guarantee you 100%
+            anonymity in voting process.
+          </p>
+          
+        </div>
+      </div> */}
     </div>
   );
 }
